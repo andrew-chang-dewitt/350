62c36e18698381655049dfef524b4445b15faf00
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2025-02-04 13:38:29 -0600

    feat(testing): add Result collection to framework

--- src/ctest/lib.h
+++ src/ctest/lib.h
@@ -7,17 +7,17 @@
 #include "../llist.h"
 #include "../result.h"
 
-typedef struct Empty {
+typedef struct {
 } Empty;
 
-struct Failure {
+typedef struct {
   char *test;
   char *assertion;
   char *file;
   int line;
-};
+} Failure;
 
-DEFINE_RESULT(Empty, struct Failure, TestResult)
+DEFINE_RESULT(Empty, Failure, TestResult)
 
 typedef struct {
   char *name;
@@ -27,36 +27,32 @@ typedef struct {
 
 declare_vec(Test);
 
-#ifdef TEST_IMPL_WITH_MAIN
-#include "main.c"
-#endif
+declare_vec(Failure);
 
-// declare_vec(Failure);
-//
-// struct Report {
-//   int pass;
-//   struct Vec_Failure failures;
+typedef struct {
+  int pass;
+  Vec_Failure failures;
+} Report;
+
+// TODO: define these now so that tests will get registered & their bodies will
+// be called by Node->exec()
+
+// replace test w/ function that registers test onto an object
+// struct Node {
+//   struct Node *next;
+//   struct Node *prev;
+//   TestResult (*exec)();
+//   char *file;
+//   char *name;
 // };
 
-// // TODO: define these now so that tests will get registered & their bodies
-// will
-// // be called by Node->exec()
-//
-// // replace test w/ function that registers test onto an object
-// // struct Node {
-// //   struct Node *next;
-// //   struct Node *prev;
-// //   TestResult (*exec)();
-// //   char *file;
-// //   char *name;
-// // };
 // #define TEST(name, fn) \
 //   _list_append(&root, { \
 //                           .name = name, \
 //                           .exec = fn, \
 //                           .file = __FILE__, \
 //                       })
-//
+
 // #define ASSERT(ast) \
 //   // TODO: need to somehow give an id to this function? not sure what to
 //   do...
@@ -70,4 +66,9 @@ declare_vec(Test);
 // //       putchar('F'); \
 // //     } \
 // //   } while (0)
+
+#ifdef TEST_IMPL_WITH_MAIN
+#include "main.c"
+#endif
+
 #endif
--- src/ctest/main.c
+++ src/ctest/main.c
@@ -1,30 +1,17 @@
-#ifdef TEST_IMPL_WITH_MAIN
 #include "lib.h"
 
-#define TEST_MAIN                                                              \
-  int main(int _, char **__) {                                                 \
-    Vec_Test tests = vec_new(Test, NULL);                                      \
-    printf("\n\n[ctest] Running tests...\n\n");                                \
-    /*int exit = run(&tests, &report);*/                                       \
-    int exit = run(&tests);                                                    \
-    printf("[ctest] Done.\n");                                                 \
-    return exit;                                                               \
-  }
-
-// static void exec_test(struct Node *test, struct Report *report) {
-static void exec_test(Test *test) {
+static void exec_test(Test *test, Report *report) {
   TestResult result = test->run();
   if (Result_is_ok(&result)) {
     putchar('.');
-    // report->pass++;
-  } /*else {
+    report->pass++;
+  } else {
     Failure f = Result_unwrap_err(&result);
-    // report->failures[0] = f;
-  }*/
+    report->failures.vtable->append(&report->failures, f);
+  }
 }
 
-// static int run(struct List *tests, struct Report *report) {
-static int run(Vec_Test *tests) {
+static int run(Vec_Test *tests, Report *report) {
   if (!tests->len) {
     printf("[ctest] Warning: No tests found\n\n");
     return 0;
@@ -33,14 +20,32 @@ static int run(Vec_Test *tests) {
   Iterable_Test i = tests->vtable->iter(tests);
   Test *test = i.vtable->next(&i);
   while (test) {
-    // exec_test(test, report);
-    exec_test(test);
+    exec_test(test, report);
     test = i.vtable->next(&i);
   };
 
-  // putchar('\n');
   return 0;
 }
 
-TEST_MAIN
+// TODO: playing with having the SUITE macro expand to a main fn that builds
+// the `tests` vec in place from the Test objs created by each TEST macro
+// thinking it could be done by having SUITE iterate over __VA_ARGS__,
+// declaring a function for the BODY given to each TEST & creating a Test
+// object that receives a pointer to that function, then appending that Test
+// object to the `tests` vec each time.
+#ifdef TEST_IMPL_WITH_MAIN
+/*
+#define SUITE(name, ...)                                                       \
+ */
+#define SUITE(name)                                                            \
+  int main(int _, char **__) {                                                 \
+    Vec_Test tests = vec_new(Test, NULL);                                      \
+    Report report = {.pass = 0, .failures = vec_new(Failure, NULL)};           \
+    printf("\n\n[ctest] Running tests...\n\n");                                \
+    int exit = run(&tests, &report);                                           \
+    printf("[ctest] Done.\n");                                                 \
+    tests.vtable->destroy(tests);                                              \
+    report.failures.vtable->destroy(report.failures);                          \
+    return exit;                                                               \
+  }
 #endif
